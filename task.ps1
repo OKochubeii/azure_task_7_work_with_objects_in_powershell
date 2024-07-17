ddf6aaebd08517d2a8ca1adec39690c94ff72b14
alexk <alexkochubey13@gmail.com>
2024-07-17 19:11:57 +0300

    Solution?

--- task.ps1
+++ task.ps1
@@ -1,1 +1,16 @@
-# Write your code here
+$dataPath = ".\data"
+$vmSize = "Standard_B2pts_v2"
+$resultRegions = @()
+
+$filesList = Get-ChildItem -Path $dataPath
+
+foreach ($file in $filesList) {
+    $fileContent = Get-Content -Path $file.FullName -Raw | ConvertFrom-Json
+
+    if ($fileContent | Where-Object { $_.name -eq $vmSize }) {
+        $resultRegions += $file.BaseName
+    }
+}
+
+$resultFile = ".\result"
+$resultRegions | ConvertTo-Json -Depth 1 | Out-File -FilePath $resultFile -Encoding utf8
